90ad2c91cee9d878d1b9198a684515ef92ce695b
Dhaval Patel <dhavalp.dev@gmail.com>
2022-09-26 09:08:01 +0530

    cores origin mini fix

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const useRouter = require("./routes/router");
 
 const app = express();
 
+const origin = process.env.FRONTEND_URL;
+
 //app uses
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser({
+  origin,
+}));
 app.use(useRouter);
 
 const port = process.env.PORT || 5000;
@@ -26,13 +30,13 @@ app.use((req, res, next) => {
 });
 
 
-if (process.env.NODE_ENV == "production") {
+/* if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+} */
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
