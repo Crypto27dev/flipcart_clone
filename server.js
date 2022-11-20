a7978060c58d68215d1c4e4923343e2e20a9d116
Dhaval Patel <dhavalp.dev@gmail.com>
2022-11-20 15:53:56 +0530

    #

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ if (process.env.NODE_ENV == "production") {
     express.static(path.join(__dirname, "client", "build", "manifest.json"))
   );
 
-  app.get("/", (req, res) => {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
